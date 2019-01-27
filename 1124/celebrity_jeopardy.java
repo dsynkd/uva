2652d2d3f2b8e3985c1d063fa8d95137697c957e
dsynkd <darrensadr@gmail.com>
2019-01-27 15:25:01 -0600

    celebrity jeopardy

---
+++ 1124/celebrity_jeopardy.java
@@ -0,0 +1,17 @@
+/* @JUDGE_ID: darrensadr 272 JAVA */ 
+
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+	public static void main (String args[]) throws Exception
+	{
+		String input;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		Boolean osc = true;
+		while ((input = reader.readLine()) != null) {
+			System.out.println(input);
+		}
+	}
+}
